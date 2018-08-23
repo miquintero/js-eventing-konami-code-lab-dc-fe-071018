fd79ae78c8bb2e4189b1895dfbb62bcc97be7ace
Alexa Schaeffer <alps231@gmail.com>
2018-08-23 02:21:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0
 
 function init() {
-  const body = document.body
+  const body = document.getElementbyId('body')
 
   const key = parseInt(e.detail || e.which);
 
