7ead682f19f5d23da67dea7907f0b9b0315fb136
Alexa Schaeffer <alps231@gmail.com>
2018-08-23 02:11:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  const document.body
+  body.addEventListener("keydown", function(event) {
+    alert("You did awesome!")
+  }
+  return e.preventDefault()
   // Write your JavaScript code inside the init() function
-
 }
